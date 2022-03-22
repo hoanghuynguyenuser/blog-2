cf401c9fa0485420f1d7b724edee8ad305180f11
Hoang Huy Nguyen <hoanghuynguyen.info@gmail.com>
2022-03-22 23:06:57 +0700

    C4 - Complete [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,17 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose')
+
+
 class SiteController {
     //[GET] /home
-    home(req, res) {
-        // res.render('home');
-        // res.json({
-        //     name: 'test'
-        // });
-
-        Course.find({}, function(err, courses) {
-            if (!err) {res.json(courses)}
-            else { res.status(400).json({error: 'ERROR'}) }
-            
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
 
     }
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -6,6 +6,6 @@ const siteController = require('../app/controllers/SiteController');
 // newsController.index
 
 router.get('/search', siteController.search);
-router.get('/', siteController.home);
+router.get('/', siteController.index);
 
 module.exports = router;
