4d407deea0f61052ee258d2e48d52b3fe57e7cac
Hoang Huy Nguyen <hoanghuynguyen.info@gmail.com>
2022-03-25 18:30:47 +0700

    C13 - Sort middleware

--- src/index.js
+++ src/index.js
@@ -5,11 +5,35 @@ const exphbs = require('express-handlebars');
 const methodOverride = require('method-override')
 const hbs = exphbs.create({ extname: '.hbs',
     helpers: {
-        sum: (a, b) => a + b 
+        sum: (a, b) => a + b,
+        sortable: (field, sort) => {
+            const sortType = field === sort.column ? sort.type : 'default'
+
+            const icons = {
+                default: 'oi oi-elevator',
+                asc: 'oi oi-sort-ascending',
+                desc: 'oi oi-sort-descending'
+            }
+
+            const types = {
+                default: 'desc',
+                asc: 'desc',
+                desc: 'asc'
+            }
+
+            const icon = icons[sortType]
+            const type = types[sortType]
+
+            return `<a href="?_sort&column=${field}&type=${type}" >
+                <span class="${icon}"></span>
+            </a>`
+        }
 }
 });
 const app = express();
 const port = 3000;
+
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
 
 const route = require('./routes');
 const db = require('./config/db');
@@ -27,6 +51,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(methodOverride('_method'))
+
+//Custom middlewares
+app.use(SortMiddleware)
 
 //Route init
 route(app);
